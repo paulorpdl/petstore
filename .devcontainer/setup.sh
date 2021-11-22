822c7e2abbb2e1ac5ae729e7777cdae1552e98c7
Paulo Pinto <ppinto@gbm.net>
2021-11-22 13:35:35 -0500

    Enabiling repo for golang codespace

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,34 @@
+!#/bin/bash
+
+dnf upgrade -y
+dnf clean all
+
+useradd -m user
+su user
+
+# install go
+VERSION='1.17'
+OS='linux'
+ARCH='amd64'
+
+curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
+tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
+
+go get -u golang.org/x/tools/gopls
+go get -u github.com/go-delve/delve/cmd/dlv
+go get -u github.com/rakyll/hey
+
+go get -u -v github.com/acroca/go-symbols
+go get -u -v github.com/cweill/gotests/...
+go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u -v github.com/haya14busa/goplay/cmd/goplay
+go get -u -v github.com/mdempsky/gocode
+go get -u -v github.com/ramya-rao-a/go-outline
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/sqs/goreturns
+go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u -v github.com/zmb3/gogetdoc
+go get -u -v golang.org/x/lint/golint
+go get -u -v golang.org/x/tools/cmd/gorename
+
